2a53fff25e49c3fd699d679b1c249df0dea4233f
sarathblackz <sarathappu0000@gmail.com>
2023-07-03 18:35:08 +0530

    New

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="child1">
+        <div className="card">
+          <div className="parent1">
+            <h6>FREE</h6>
+            <h1>$0<span className="period">/month</span></h1>
+          </div>
+          <hr></hr>
+          <div className="parent2">
+            <p><span>✔</span> Single User</p>
+            <p><span>✔</span> 5GB Storage</p>
+            <p><span>✔</span> Unlimited public projects</p>
+            <p><span>✔</span> Community Access</p>
+            <p><span>✖</span> Unlimited Private Projects</p>
+            <p><span>✖</span> Dedicated phone Support</p>
+            <p><span>✖</span> Free Subdomain</p>
+            <p><span>✖</span> Monthly Status Reports</p>
+          </div>
+          <button>BUTTON</button>
+        </div>
+      </div>
+      <div className="child2">
+        <div className="card">
+          <div className="parent1">
+            <h6>PLUS</h6>
+            <h1>$9<span className="period">/month</span></h1>
+          </div>
+          <hr></hr>
+          <div className="parent2">
+            <p><span>✔</span> 5 User</p>
+            <p><span>✔</span> 50GB Storage</p>
+            <p><span>✔</span> Unlimited public projects</p>
+            <p><span>✔</span> Community Access</p>
+            <p><span>✔</span> Unlimited Private Projects</p>
+            <p><span>✔</span> Dedicated phone Support</p>
+            <p><span>✔</span> Free Subdomain</p>
+            <p><span>✖</span> Monthly Status Reports</p>
+          </div>
+          <button>BUTTON</button>
+        </div>
+      </div>
+      <div className="child3">
+        <div className="card">
+          <div className="parent1">
+            <h6>PRO</h6>
+            <h1>$49<span className="period">/month</span></h1>
+          </div>
+          <hr></hr>
+          <div className="parent2">
+            <p><span>✔</span> Unlimited User</p>
+            <p><span>✔</span> 150GB Storage</p>
+            <p><span>✔</span> Unlimited public projects</p>
+            <p><span>✔</span> Community Access</p>
+            <p><span>✔</span> Unlimited Private Projects</p>
+            <p><span>✔</span> Dedicated phone Support</p>
+            <p><span>✔</span> Unlimited Subdomain</p>
+            <p><span>✔</span> Monthly Status Reports</p>
+          </div>
+          <button>BUTTON</button>
+        </div>
+      </div>
     </div>
   );
 }
